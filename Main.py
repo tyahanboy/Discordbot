d9e95a5b044814060f7377c2d59d6dcc9923052a
tyahanboy <koki20101004@gmail.com>
2026-06-23 23:07:12 +0900

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run("DISCORD_BOT_TOKEN")
